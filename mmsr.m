963b39f8f964bccb3ffe616228d277c8c65d0a65
lucaslie <lucasl@mit.edu>
2016-12-09 12:58:58 -0500

    added some links for rubber-like stress models

--- mmsr.m
+++ mmsr.m
@@ -1,2 +1,7 @@
+% Hyperelastic Material properties
+% https://en.wikipedia.org/wiki/Hyperelastic_material
+% https://en.wikipedia.org/wiki/Mooney%E2%80%93Rivlin_solid
+% https://en.wikipedia.org/wiki/Neo-Hookean_solid
+
 csr();
 tm();
