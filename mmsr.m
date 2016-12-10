38dc7d52d08ba0af67833d4269ecd2994f95c381
Robert Katzschmann <rkk@csail.mit.edu>
2016-12-10 09:53:41 -0500

    expand mmsr function

--- mmsr.m
+++ mmsr.m
@@ -2,6 +2,8 @@
 % https://en.wikipedia.org/wiki/Hyperelastic_material
 % https://en.wikipedia.org/wiki/Mooney%E2%80%93Rivlin_solid
 % https://en.wikipedia.org/wiki/Neo-Hookean_solid
-
-y = csr();
-tm();
+[u,y] = load()
+K = 2;
+csrObj = Csr();
+[functions,variances,gamma] = runEM(obj,u,y,K);
+[transitions] = runTM(gamma,K);
