a27a946b50aff40be82c97f28d8dff2d1e712aa1
lucaslie <lucasl@mit.edu>
2016-12-10 09:29:46 -0500

    added first round of symbolic regression

--- @Csr/runEM.m
+++ @Csr/runEM.m
@@ -4,21 +4,46 @@
 %   the number of subfunctions - K
 % output: 
 %   behavior for each mode - f_k (u_n)
-%   variance for each mode - sigma^2_k
+%   variance for each mode - sigma^2_k, var_k
 
-function [f,var] = runEM(obj,u,y,K)
+function [f,var] = runEM(obj)
 
+% making sure we don't run the EM algorithm without initialization
+errorMsg = 'You cannot start the EM-algorithm without initialization! Aborting';
+assert(obj.initiated == true,errorMsg);
 % making sure we don't run the EM algorithm multiple times at the same time
-errorMsg = 'You cannot initialize the EM-algorithm twice! Aborting';
+errorMsg = 'You cannot start the EM-algorithm twice! Aborting';
 assert(obj.runningEM == false,errorMsg);
 obj.runningEM = true;
+
+% get some variables ready 
+f = cell(obj.K,1);
+var_k = zeros(obj.K,1);
 
 % initialize random membership values
 
 % for each behavior in K modes :
+for k = 1:obj.K
+    obj.k_current = k;
+    
     % sr_solutions = symbolic_regression(y_n = f_k(u_n),fitfunc)
-    % returns pareto set
-    % set behavior f k to solution with lowest local AIC score in sr_solutions
+    [gp,index] = obj.symReg(); % returns pareto set
+    
+    % check out best solution
+    aic = zeros(size(index));
+    aic(index == 0) = Inf;
+        for i = 1:size(index,1)
+            if index(i) == 0
+                continue;
+            end
+            aic(i) = obj.computeLocalAIC(gp,i,k);
+        end
+    
+    % set behavior f_k to solution with lowest local AIC score in sr_solutions
+    [aic_best,i_best] = min(aic);
+    f{k} = gpmodel2sym(gp,i_best);
+    
+end
 
 % set variance for each behavior - sigma^2_k (Equation 5)
 
