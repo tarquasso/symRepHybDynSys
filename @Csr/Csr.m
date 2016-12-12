a60e1ae8664b8d14154991b49accd33c674371e3
lucaslie <lucasl@mit.edu>
2016-12-12 13:31:37 -0500

    added optimized function for absolute error without for loops

--- @Csr/Csr.m
+++ @Csr/Csr.m
@@ -37,6 +37,7 @@ classdef (Sealed) Csr < handle
         aic = computeAIC(obj,gp,index,k)
         ecsr_k = kAbsError(obj,ypred,yactual,weights);
         ecsr_k = kQuadError(obj,ypred,yactual,weights);
+        ecsr = absError(obj,ypred,yactual,weights);
         var = computeVar(obj,k,gp,i,set,gamma);
         yhat = predictData(obj,k,x);
         gamma = computeGamma(obj,k,var,x,y);
---
+++ @Csr/absError.m
@@ -0,0 +1,14 @@
+function ecsr = absError(obj,ypred,yactual,weights)
+% compute E_Csr where sum over all modes and the whole data set.
+
+% ypred = n x k 
+% yactual = n x 1
+% weights = k x n
+% E_csr = sum_k sum_n gamma_kn*abs(y_n - ypred_kn)
+
+err = abs(ypred - repmat(yactual,1,obj.k));
+ecsr_kn = weights.*err';
+ecsr = sum(ecsr_kn(:));
+
+end
+
