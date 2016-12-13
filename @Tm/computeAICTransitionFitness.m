f2c391a672b190f7fb2fda10f6a01e1061fce9c2
lucaslie <lucasl@mit.edu>
2016-12-13 15:05:10 -0500

    bug fix

--- @Tm/computeAICTransitionFitness.m
+++ @Tm/computeAICTransitionFitness.m
@@ -4,7 +4,7 @@ c = gp.fitness.nodecount(index);
 N = size(obj.y_val,1);
 gpmodel = gpmodel2struct(gp,index);
 ypred = gpmodel.val.ypred;
-yactual = obj.y_val;
+yactual = gp.userdata.yval;
 gammatilde = obj.getGammaTildeVal(obj.k_current);
 
 aic = 2*c + N*log(obj.transitionFitness(ypred,yactual,gammatilde));
