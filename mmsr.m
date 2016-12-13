707b546acfac09c150a1ccefc00fead87c967b07
Robert Katzschmann <rkk@csail.mit.edu>
2016-12-13 11:52:13 -0500

    update mmsr

--- mmsr.m
+++ mmsr.m
@@ -40,41 +40,85 @@ end
 
 %% CSR
 
-% csrObj = Csr.getInstance();
-% [functions,variances,gamma] = csrObj.runEM(obj,u,y,K);
-
-
-%% Generate Gamma:
-gamma = zeros(K,N); %KxN matrix
-
-idxMode1 = (modeTrain ==1);
-gamma(1,idxMode1) = 1;
-
-idxMode2 = (modeTrain == 2);
-gamma(2,idxMode2) = 1;
-
-figure;
-plot(1:N,gamma,'-..')
+if(false)
 
 %% Set up Init File
-init.x_train = xTrain;
-init.y_train = gammaTrain; % seprate it out in runTM
+initCsr.x_train = xTrain;
+initCsr.y_train = yTrain; % seprate it out in runTM
 
-init.x_val = xVal;
-init.y_val = gammaVal;
+initCsr.x_val = xVal;
+initCsr.y_val = yVal;
 
-init.x_test = xTest;
-init.y_test = gammaTest;
+initCsr.x_test = xTest;
+initCsr.y_test = yTest;
 
-init.K = K;
+initCsr.K = K;
+  
+csrObj = Csr.getInstance();
+csrObj.initiateCsr(initCsr);
 
+% [functions,variances] = csrObj.runEM();
+
+gammaTrain = csrObj.gamma_train;
+gammaVal = csrObj.gamma_val;
+gammaTest = csrObj.gamma_test;    
+
+else
+  
+%% Generate Gamma:
+gammaTrain = zeros(K,N); %KxN matrix
+gammaTrain(1, (modeTrain == 1) ) = 1;
+gammaTrain(2, (modeTrain == 2) ) = 1;
+
+gammaVal = zeros(K,N); %KxN matrix
+gammaVal(1, (modeVal == 1) ) = 1;
+gammaVal(2, (modeVal == 2) ) = 1;
+
+gammaTest = zeros(K,N); %KxN matrix
+gammaTest(1, (modeTest == 1) ) = 1;
+gammaTest(2, (modeTest == 2) ) = 1;
+
+end
+% 
+% figure(1); clf;
+% plot(1:N,gammaTrain,'-..')
+% 
+% figure(2); clf;
+% plot(1:N,gammaVal,'-..')
+% 
+% figure(3); clf;
+% plot(1:N,gammaTest,'-..')
+
+%% Set up Init File
+initTm.x_train = xTrain;
+initTm.y_train = gammaTrain; % seprate it out in runTM
+
+initTm.x_val = xVal;
+initTm.y_val = gammaVal;
+
+initTm.x_test = xTest;
+initTm.y_test = gammaTest;
+
+initTm.K = K;
 
 tmObj = Tm.getInstance();
 
-
 tmObj.initiateTm(initTm);
 
-return
-%%
-tmObj.in
-[transitions] = tmObj.runTM(gamma,K);
+%% Run the TM Algorithm
+tmObj.runTM();
+
+%% Evaluate the output
+k=1;
+ksub=2;
+f_1_2 = tmObj.getAlgebraicFunction(obj,k,ksub);
+transTrain_1_2 = getPredictedTransitionsTrain(obj,k,ksub);
+transVal_1_2 = getPredictedTransitionsVal(obj,k,ksub);
+transTest_1_2 = getPredictedTransitionsTest(obj,k,ksub);
+
+k=2;
+ksub=1;
+f_2_1 = tmObj.getAlgebraicFunction(obj,k,ksub);
+transTrain_2_1 = getPredictedTransitionsTrain(obj,k,ksub);
+transVal_2_1 = getPredictedTransitionsVal(obj,k,ksub);
+transTest_2_1 = getPredictedTransitionsTest(obj,k,ksub);
