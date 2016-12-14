6ad5531bb7396d201a76493456205531d66f0f51
lucaslie <lucasl@mit.edu>
2016-12-14 00:35:10 -0500

    reordered and cleaned up gpConfigTm. Note that field order corresponds to order in gpdefaults now

--- @Tm/gpConfigTM.m
+++ @Tm/gpConfigTM.m
@@ -2,10 +2,10 @@ function gp = gpConfigTM(obj,gp)
 
     % runcontrol
     gp.runcontrol.pop_size = 64; %population size
-    gp.runcontrol.num_gen = 1000; % number of generations - paper used 20000
+    gp.runcontrol.num_gen = 2000; % number of generations - paper used 20000
     gp.runcontrol.showBestInputs = true; 
     gp.runcontrol.showValBestInputs = true;
-    gp.runcontrol.timeout = 30; %adjust?
+    gp.runcontrol.timeout = inf; %adjust?
     gp.runcontrol.runs = 1; %single run per transition
     gp.runcontrol.parallel.auto = false; %potentially faster
     gp.runcontrol.verbose = 100; 
@@ -16,61 +16,41 @@ function gp = gpConfigTM(obj,gp)
     gp.selection.elite_fraction = 0.3;
     gp.nodes.const.p_int = 0.5;
     tmobj = Tm.getInstance;
+    
     % fitness
     gp.fitness.minimisation = true;         %true to minimise the fitness function (if false it is maximised).
-    gp.fitness.fitfun = @tmobj.tmFitfun;
+    gp.fitness.fitfun = @tmobj.tmFitfun; 		         
     
-    
-    %input configuration 
-    gp.nodes.inputs.num_inp = 1; 		         
-
-    % multigene
-    gp.genes.max_genes = 2;
-    
-    % constants
-    gp.nodes.const.p_ERC = 0.05;
-    
-    % tree information
-    gp.treedef.max_depth = 3;
-    
-    % data
-    gp.userdata.xtrain = obj.x_train;
-    gp.userdata.ytrain = obj.y_train(:,obj.ksub_current);
-    gp.userdata.xval   = obj.x_val;
-    gp.userdata.yval   = obj.y_val(:,obj.ksub_current);
-    gp.userdata.xtest  = obj.x_test;
-    gp.userdata.ytest  = obj.y_test(:,obj.ksub_current);
-    [gp.userdata.numytrain, gp.nodes.inputs.num_inp] = size(gp.userdata.xtrain);
-    
-    if size(gp.userdata.xtrain,1) ~= size(gp.userdata.ytrain,1)
-        error('There must be the same number of rows in gp.userdata.xtrain and gp.userdata.ytrain');
-    end
-    
-    if any(any(~isfinite(gp.userdata.xtrain))) || any(any(~isfinite(gp.userdata.ytrain)))
-        error('Non-finite values detected in gp.userdata.xtrain or gp.userdata.ytrain');
-    end
-    
-    if any(any(~isreal(gp.userdata.xtrain))) || any(any(~isreal(gp.userdata.ytrain)))
-        error('Complex values detected in gp.userdata.xtrain or gp.userdata.ytrain');
-    end
-    
+    % data, naming convention is different for single tree
     gp.userdata.name = 'Transitions';
-    
-    
+    gp.userdata.x = obj.x_train;
+    gp.userdata.y = obj.y_train(:,obj.ksub_current);
+    %gp.userdata.xtrain = obj.x_train;
+    %gp.userdata.ytrain = obj.y_train(:,obj.ksub_current);
+    %gp.userdata.xval   = obj.x_val;
+    %gp.userdata.yval   = obj.y_val(:,obj.ksub_current);
+    %gp.userdata.xtest  = obj.x_test;
+    %gp.userdata.ytest  = obj.y_test(:,obj.ksub_current);
     %enables hold out validation set
     %gp.userdata.user_fcn =  @tmobj.tmFitfunValidate;
+    [gp.userdata.numytrain, gp.nodes.inputs.num_inp] = size(gp.userdata.x);
     
-    % variable names
-    %gp.nodes.inputs.names = {'z','dz','theta','dtheta'};
+    if size(gp.userdata.x,1) ~= size(gp.userdata.y,1)
+        error('There must be the same number of rows in gp.userdata.x and gp.userdata.y');
+    end
     
-    % TODO: add termination criterion 
-    % the maximum amount of time to run for (in seconds) 
-    % can be set for each run as well as a target fitness.
-    % E.g. for multigene regression the target fitness can be set as model
-    % root mean squared error (RMSE) on the training data
-    % gp.fitness.terminate = true;
-    % gp.fitness.terminate_value = 0.2;
-
+    if any(any(~isfinite(gp.userdata.x))) || any(any(~isfinite(gp.userdata.y)))
+        error('Non-finite values detected in gp.userdata.xtrain or gp.userdata.y');
+    end
+    
+    if any(any(~isreal(gp.userdata.x))) || any(any(~isreal(gp.userdata.y)))
+        error('Complex values detected in gp.userdata.x or gp.userdata.y');
+    end
+    
+    % tree information (make it possibly deeper since we only have one tree)
+    gp.treedef.max_depth = 12;
+    gp.treedef.max_mutate_depth = 7;
+    
     %genetic operators
     gp.operators.mutation.p_mutate = 0.03;    
     gp.operators.crossover.p_cross = 0.70;
@@ -78,9 +58,14 @@ function gp = gpConfigTM(obj,gp)
     
     % function nodes
     gp.nodes.functions.name = {'times','minus','plus','rdivide'};
+    %gp.nodes.functions.name = {'times','minus','plus','rdivide','square',...
+    %    'mult3','add3','sqrt','cube','neg'};
     
-%     gp.nodes.functions.name = {'times','minus','plus','rdivide','square',...
-%         'mult3','add3','sqrt','cube','neg'};
-      
+    % constants
+    gp.nodes.const.p_ERC = 0.05;
+    
+    % multigene - NO
+    gp.genes.multigene = false;
+    gp.genes.max_genes = 1;
     
 end
