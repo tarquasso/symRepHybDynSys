6f6a2b4b6a9fc2d03bfdd2e49a7d796403858db1
lucaslie <lucasl@mit.edu>
2016-12-13 23:32:52 -0500

    refactored fitness function into much simpler version

--- @Tm/transitionFitness.m
+++ @Tm/transitionFitness.m
@@ -1,12 +1,13 @@
 function fitness = transitionFitness(ypred,yactual,gammatilde)
 
-if(~isempty(ypred))
+if(size(ypred) == size (yactual))
   err = yactual(2:end) - logsig(ypred(1:end-1)); % calculate error based on validation - prediction
   fitness = sum(gammatilde .* (err.^2));
   
   sumGammaTilde = sum(gammatilde);
   fitness = fitness/sumGammaTilde; %denominator operation of eq. 18, normalizes
 else
+  warning('Invalid predictions, setting fitness to Inf');
   fitness = inf;
 end
 end
