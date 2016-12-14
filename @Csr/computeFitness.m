2b81ca78aa5ce26b40fa33715870fa8c70b92d1a
lucaslie <lucasl@mit.edu>
2016-12-14 16:57:54 -0500

    new fitness computation

--- @Csr/computeFitness.m
+++ @Csr/computeFitness.m
@@ -1,6 +1,19 @@
 function fitness = computeFitness(weights,y,ypred)
-%calculate weighted, mean logarithmic error
-fitness = ( weights*log(1+abs(y - ypred)) ) / sum(weights);
+
+% checking for correct size and if a elements are finite reals
+% This check is necessary since there is a small number of models that are
+% generated and just bullshit (less than 1%, checked)
+check_size = isequal(size(ypred),size(yactual));
+check_valid = all( isfinite( ypred(:) ));
+check_real = all( isreal( ypred(:) ));
+
+if check_size && check_valid && check_real
+    %calculate weighted, mean logarithmic error
+    fitness = ( weights*log(1+abs(y - ypred)) ) / sum(weights);
+else    
+    fitness = inf;   
+    %   warning('Invalid predictions, setting fitness to Inf');    
+end
 
 end
 
