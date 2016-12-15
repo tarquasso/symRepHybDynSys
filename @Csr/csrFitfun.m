5eca67b148c089076910e2e325b5cf5ad29ea1ea
lucaslie <lucasl@mit.edu>
2016-12-14 20:32:33 -0500

    big fix

--- @Csr/csrFitfun.m
+++ @Csr/csrFitfun.m
@@ -1,5 +1,5 @@
 function [fitness,gp] = csrFitfun(evalstr,gp)
-% fitness for our TM single-gene usage gptips.
+% fitness for our CSR single-gene usage gptips.
 
 % extract x and y data from GP struct
 x = gp.userdata.xtrain; % = n x dim
@@ -9,7 +9,7 @@ ytrain = gp.userdata.ytrain;  % = n x 1
 ypred = Csr.predictData(evalstr,x);
 
 % fitness
-gamma = Tm.getInstance.getWeightsTrain();
-fitness = Tm.transitionFitness(gamma,ytrain,ypred);
+gamma = Csr.getInstance.getWeightsTrain();
+fitness = Csr.computeFitness(gamma,ytrain,ypred);
 
 end
