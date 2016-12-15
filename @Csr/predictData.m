88ef19a538068d7862c01a58ec0fcde9059d857c
lucaslie <lucasl@mit.edu>
2016-12-14 20:38:25 -0500

    new predict function

--- @Csr/predictData.m
+++ @Csr/predictData.m
@@ -1,35 +1,40 @@
-function yhat = predictData(obj,k,x)
+function ypred = predictData(predictor,x,index)
 
-if strcmpi(k,'all')
-    k = ':';
+% assume we call predictData with a gp struct
+% then we need to provide the index for which model in the population we
+% want to predict (that's why we check for nargin == 3).
+if isa(predictor,'struct') && nargin == 3
+    evalstr = tree2evalstr(predictor.pop{index},predictor);
+    
+% elsewise we can call it with a symbolic function object, but then we
+% cannot give an index, which model we should use. The appropriate sym
+% object must be checked for beforehand when calling it.
+elseif isa(predictor,'sym') && nargin == 2
+    evalstr{1} = f; % don't know if that is going work due to the dimensions
+    
+% In this case we already provide the cell array containing the function
+% char-array. This arises when we do the call to 'tree2evalstr' before
+% giving it to predictDataTM.
+elseif isa(predictor,'cell') && nargin == 2
+    evalstr = predictor;
+    
+else
+    error('Check how to call predictData correctly');
 end
 
-if strcmpi(x,'val') || isequal(x,obj.x_val)
-    yhat = obj.ypred_val(:,k);
-    return;
-end
-
-if strcmpi(x,'test') || isequal(x,obj.x_test)
-    yhat = obj.ypred_test(:,k);
-    return;
-end
-
-if strcmpi(x,'train') || isequal(x,obj.x_train)
-    yhat = obj.ypred_train(:,k);
-    return;
-end
-
-assert(~(ischar(k) && ~ischar(x)),'You need to set x = {test,train,val} when you use k = all');
 dim = size(x,2);
-xsym = sym('x',[1 dim]);
-contained = symvar(obj.f{k});
-dim_reduced = length(contained);
-x_important = cell(dim_reduced,1);
-for i=1:dim_reduced
-    var = find(xsym == contained(i));
-    x_important{i} = x(:,var);
+n = size(x,1);
+
+for d = 1:dim
+    string = ['x' num2str(d) ' = x(:,' num2str(d) ');'];
+    eval(string);
 end
-g = matlabFunction(f);
-yhat = g(x_important{:});
+
+% preallocate and use (:) to ensure we populate the whole vector
+% consider the case where evalstr{1} is only a constant. Output will be a
+% scalar. With this operation, we ensure that this scalar is mapped to all
+% of the ypred vector.
+ypred = zeros(n,1);
+ypred(:) = eval(evalstr{1});
 
 end
