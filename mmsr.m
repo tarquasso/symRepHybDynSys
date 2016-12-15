132c680b45246c3a51bade2fd870bd240b636363
lucaslie <lucasl@mit.edu>
2016-12-15 02:27:41 -0500

    Merge

--- mmsr.m
+++ mmsr.m
@@ -16,6 +16,22 @@ if(true)
 
 K = 2;
 % dataTrain.tAll
+
+%NTrain = length(dataTrain.zAll);
+%xTrain = [dataTrain.zAll];%,dataTrain.zdAll];
+%yTrain = dataTrain.zddAll;
+%modeTrain = dataTrain.mode; % 1 is in the air, 2 is in contact
+
+%NVal = length(dataVal.zAll);
+%xVal = [dataVal.zAll];%,dataVal.zdAll];
+%yVal = dataVal.zddAll;
+%modeVal = dataVal.mode; % 1 is in the air, 2 is in contact
+
+%NTest = length(dataTest.zAll);
+%xTest = [dataTest.zAll];%,dataTest.zdAll];
+%yTest = dataTest.zddAll;
+%modeTest = dataTest.mode; % 1 is in the air, 2 is in contact
+
 xTrain = [dataTrain.zAll,dataTrain.zdAll];
 yTrain = dataTrain.zddAll;
 mTrain = dataTrain.mode; % 1 is in the air, 2 is in contact
@@ -57,40 +73,6 @@ modeTest = dataTest.m'; %for comparision
 
 end
 
-%% CSR
-
-if(true)
-
-%% Set up Init File
-initCsr.x_train = xTrain;
-initCsr.y_train = yTrain; % seprate it out in runTM
-
-initCsr.x_val = xVal;
-initCsr.y_val = yVal;
-
-initCsr.x_test = xTest;
-initCsr.y_test = yTest;
-
-initCsr.K = K;
-  
-csrObj = Csr.getInstance();
-csrObj.initiateCsr(initCsr);
-
-[functions,variances] = csrObj.runEM();
-
-% one hot encoding of modes
-[~,gammaTrain] = max(csrObj.gamma_train,[],1);
-[~,gammaVal] = max(csrObj.gamma_val,[],1);
-[~,gammaTest] = max(csrObj.gamma_test,[],1);
-gammaTrain = gammaTrain';
-gammaVal = gammaVal';
-gammaTest = gammaTest';
-
-csrObj.f{1}
-csrObj.f{2}
-
-else
-  
 %% Generate Gamma:
 gammaTrain = zeros(NTrain,K); %KxN matrix
 gammaTrain((modeTrain == 1),1) = 1;
@@ -104,7 +86,46 @@ gammaTest = zeros(NTest,K); %KxN matrix
 gammaTest((modeTest == 1),1) = 1;
 gammaTest((modeTest == 2),2) = 1;
 
-end
+%% CSR
+
+
+%% Set up Init File
+initCsr.x_train = xTrain;
+initCsr.y_train = yTrain; % seprate it out in runTM
+
+initCsr.x_val = xVal;
+initCsr.y_val = yVal;
+
+initCsr.x_test = xTest;
+initCsr.y_test = yTest;
+
+initCsr.K = K;
+
+% make them deviate a little bit
+initCsr.gammaTrain = gammaTrain + 0.2*rand(NTrain,k);
+initCsr.gammaVal = gammaVal + 0.2*rand(NVal,k);
+initCsr.gammaTest = gammaTest + 0.2*rand(NTrain,k);
+
+  
+csrObj = Csr.getInstance();
+csrObj.initiateCsr(initCsr);
+
+[functions,variances] = csrObj.runEM();
+
+% one hot encoding of modes
+%[~,gammaTrain] = max(csrObj.gamma_train,[],1);
+%[~,gammaVal] = max(csrObj.gamma_val,[],1);
+%[~,gammaTest] = max(csrObj.gamma_test,[],1);
+%gammaTrain = gammaTrain';
+%gammaVal = gammaVal';
+%gammaTest = gammaTest';
+
+csrObj.f{1}
+csrObj.f{2}
+
+
+
+
 %%
 figure(1); clf;
 plot(1:NTrain,gammaTrain','-..')
