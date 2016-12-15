b89bbf7a974bd5a31df179d19cece900f9459c5d
lucaslie <lucasl@mit.edu>
2016-12-14 22:01:57 -0500

    properly adjusted AIC functions

--- @Csr/computeAIC.m
+++ @Csr/computeAIC.m
@@ -1,22 +1,27 @@
 function aic = computeAIC(obj,gp,index,k)
 
-yactual = obj.y_val;
-yhatk = obj.predictData(gp,obj.x_val,index);
+xval = obj.x_val;
+yval = obj.y_val;
+varval = obj.var_val;
+
+evalstr = tree2evalstr(obj.pareto_fstr{index},gp); % get decoded function string
+ypredvalk = obj.predictData(evalstr,xval);
 
 % for one candidate for the new pareto set
-gammahatk = obj.computeGammaHat(k,obj.var_val,obj.x_val,yactual,yhatk);
+gammahatk = obj.computeGammaHat(k,varval,xval,yval,ypredvalk);
 
 % compute global fitness using temporary values - E_csr (Equation 2)
-yhat = obj.getAllPredictions('val');
-weights = obj.getWeightsVal('all');
-weights(k,:) = gammahatk;
-yhat(:,k) = yhatk;
-ecsr = obj.absError(yhat,yactual,weights);
+ypredval = obj.getAllPredictions('val');
+ypredval(:,k) = ypredvalk;
 
+weightsval = obj.getWeightsVal('all');
+weightsval(k,:) = gammahatk;
+
+% global AIC
+ecsr = obj.absError(ypredval,yval,weightsval);
 c = gp.fitness.nodecount(index);
-N = size(yactual,1);
+N = size(yval,1);
 
-%global AIC
 aic = 2*c + N*log(ecsr);
 
 end
--- @Csr/computeLocalAIC.m
+++ @Csr/computeLocalAIC.m
@@ -1,14 +1,16 @@
 function aic = computeLocalAIC(obj,gp,index,k)
 
-c = gp.fitness.nodecount(index); % nodes for current candidate
-N = size(obj.y_val,1); % data set size
+evalstr = tree2evalstr(obj.pareto_fstr{index},gp); % get decoded function string
+ypredval = obj.predictData(evalstr,obj.x_val);
 
-ypred = obj.predictData(gp,obj.x_val,index);
-yactual = obj.y_val; % output of validation (same for all modes and candidates)
+yval = obj.y_val; % output of validation (same for all modes and candidates)
 weights = obj.getWeightsVal(k); % current mode
 
 % local AIC score eq.8 only for current gp in one mode (local)
-aic = 2*c + N*log(obj.kAbsError(ypred,yactual,weights));
+c = gp.fitness.nodecount(index); % nodes for current candidate
+N = size(obj.y_val,1); % data set size
+
+aic = 2*c + N*log(obj.kAbsError(ypredval,yval,weights));
 
 end
 
