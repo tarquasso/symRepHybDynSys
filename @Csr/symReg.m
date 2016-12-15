4420b7f0c6b8af38e57e790a2311d051b970bd04
lucaslie <lucasl@mit.edu>
2016-12-14 22:40:12 -0500

    bug fix

--- @Csr/symReg.m
+++ @Csr/symReg.m
@@ -1,5 +1,10 @@
 function gp = symReg(obj)
 
+% reset pareto variables for new run
+obj.pareto_fstr = [];
+obj.pareto_fit = [];
+obj.pareto_complex = [];
+
 % symbolic regression
 gp = rungp(@obj.gpConfig); %Running GPTIPS (p.15)
 
