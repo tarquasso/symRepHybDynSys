8f382b44abfb1e818b2003fc5ba3f52cba6f9ff6
Robert Katzschmann <rkk@csail.mit.edu>
2016-12-15 01:09:49 -0500

    fix bug with ksub

--- @Tm/fUpdate.m
+++ @Tm/fUpdate.m
@@ -9,8 +9,8 @@ obj.ypred_val(:,k,ksub)   = obj.predictDataTM(evalstr,gp.userdata.xval);
 % Our own version of gppretty to get symbolic, simplified equation out of it
 obj.f{k,ksub} = obj.tmPretty(gp,obj.pareto_fstr{i_best});
 
-obj.pareto_fstr_all{k,k_sub} = obj.pareto_fstr;
-obj.pareto_complex_all{k,k_sub} = obj.pareto_complex;
-obj.pareto_fit_all{k,k_sub} = obj.pareto_fit;
+obj.pareto_fstr_all{k,ksub} = obj.pareto_fstr;
+obj.pareto_complex_all{k,ksub} = obj.pareto_complex;
+obj.pareto_fit_all{k,ksub} = obj.pareto_fit;
 
 end
